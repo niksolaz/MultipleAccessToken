710432151308a8366f5fd83ab2ec18bc3879230a
niksolaz <solazzo.nicola@gmail.com>
2015-09-07 13:43:51 +0200

    creaadded process database

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 var database = process.env.DATABASE;
-var secret = process.env.DATABASE;
+var secret = process.env.SECRET;
 
 module.exports = {
 
     'secret': process.env.DATABASE,
-    'database': process.env.DATABASE
+    'database': process.env.SECRET
 
 };
