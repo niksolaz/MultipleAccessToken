78e570a2eaeae8d21a707fab53321957d0dfdbca
niksolaz <solazzo.nicola@gmail.com>
2015-09-07 14:15:58 +0200

    undefinied retrieving data

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
+var Database = JSON.stringify(process.env.DATABASE);
+var Secret = JSON.stringify(process.env.SECRET);
+console.log('This is database: '+Database);
+console.log('This is secret: '+Secret);
 
 module.exports = {
 
-    'secret': process.env.SECRET,
-    'database': process.env.DATABASE
+    'secret': Secret,
+    'database': Database
 
 };
