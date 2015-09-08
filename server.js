da70ee8db3fd88db30901248843a4a8c23cf50f3
niksolaz <solazzo.nicola@gmail.com>
2015-09-08 18:33:19 +0200

    get token from module oauth

--- server.js
+++ server.js
@@ -64,9 +64,9 @@ passport.deserializeUser(function(id, done) {
 });
 
 passport.use(new FacebookStrategy({
-    clientID: FACEBOOK_APP_ID,
-    clientSecret: FACEBOOK_APP_SECRET,
-    callbackURL: "http://www.example.com/auth/facebook/callback"
+    clientID: oauth.clientID,
+    clientSecret: oauth.clientSecret,
+    callbackURL: oauth.callbackURL
   },
   function(accessToken, refreshToken, profile, done) {
     User.findOne({'facebook.id':profile.id}, function(err, user) {
