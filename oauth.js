a496ea307151e177c4e7eefc2bb808ca42a752d3
niksolaz <solazzo.nicola@gmail.com>
2015-09-08 20:03:10 +0200

    processed callback URL facebook and new port

--- oauth.js
+++ oauth.js
@@ -2,7 +2,7 @@ module.exports = {
 					facebook: {
 					 clientID: process.env.FACEBOOK_APP_ID,
 					 clientSecret: process.env.FACEBOOK_APP_SECRET,
-					 callbackURL: 'http://127.0.0.1:8080/auth/facebook/callback'
+					 callbackURL: process.env.FACEBOOK_REDIRECTURI
 					},
 					twitter: {
 					 consumerKey: process.env.TWITTER_CONSUMER_KEY,
--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var User   = require('./app/models/user'); // get our mongoose model
 // =======================
 // configuration =========
 // =======================
-var port = process.env.PORT || 8080; // used to create, sign, and verify tokens
+var port = process.env.PORT || 3000; // used to create, sign, and verify tokens
 mongoose.connect(config.database); // connect to database
 app.set('superSecret', config.secret); // secret variable
 
