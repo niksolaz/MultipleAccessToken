244524a336587c03d9ed6b20d62cf618481a9d28
niksolaz <solazzo.nicola@gmail.com>
2015-09-09 15:11:52 +0200

    refactoring oauth

--- oauth.js
+++ oauth.js
@@ -1,11 +1,7 @@
 module.exports = {
 					facebook: {
-					 clientID: process.env.FACEBOOK_APP_ID,
-					 clientSecret: process.env.FACEBOOK_APP_SECRET,
-					 callbackURL: process.env.FACEBOOK_REDIRECTURI
-					},
-					twitter: {
-					 consumerKey: process.env.TWITTER_CONSUMER_KEY,
-					 consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-					 callbackURL: "http://127.0.0.1:8080/auth/twitter/callback"
+					 'clientID': process.env.FACEBOOK_MAT_ID,
+					 'clientSecret': process.env.FACEBOOK_MAT_SECRET,
+					 'callbackURL': process.env.FACEBOOK_MAT_REDIRECTURI
 					}
+				}
